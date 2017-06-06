989ab54915b5715d779fc35467e595338ddecc27
Aryeh Leib Taurog <git@aryehleib.com>
2017-06-06 21:34:05 +0300

    feat: simple, working venvinstaller script

---
+++ README.rst
@@ -0,0 +1,6 @@
+venvinstaller
+==============
+
+This is a simple (probably naïve) bash script to install and configure
+a user-local virtualenv with virtualenvwrapper without requiring root
+privileges or modifying system python packages.
---
+++ venvinstaller.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+DEFAULT=~/.venv
+WORKON_HOME=$(realpath ${1:-$DEFAULT})
+VENV_VENV=$WORKON_HOME/venv
+
+get_latest_pypi_link() {
+    PACKAGE=$1
+    FORMAT=$2
+    PYPI="https://pypi.python.org"
+    URLPATH=$(
+        curl -sS $PYPI/simple/$PACKAGE/ \
+        | sed -nr "s;.*<a href=\"../../(packages/[^\"]+)\".*>${PACKAGE}-([^<]+)$FORMAT<.*;\2 \1;p" \
+        | sort -V \
+        | tail -n 1 \
+        | cut -d " " -f2
+    )
+    echo $PYPI/$URLPATH
+}
+
+add_to_bashrc() {
+    cat >> ~/.bashrc <<EOF
+
+export VIRTUALENVWRAPPER_PYTHON=$VENV_VENV/bin/python
+export VIRTUALENVWRAPPER_VIRTUALENV=$VENV_VENV/bin/virtualenv
+export VIRTUALENVWRAPPER_VIRTUALENV_CLONE=$VENV_VENV/bin/virtualenv-clone
+export WORKON_HOME=$WORKON_HOME
+source $VENV_VENV/bin/virtualenvwrapper.sh
+EOF
+}
+
+
+if [[ -e "$VENV_VENV/bin/python" ]]
+then
+    echo looks like virtualenv is already installed
+else
+    echo installing virtualenv into a virtualenv
+    mkdir -p "$WORKON_HOME"
+    cd "$WORKON_HOME"
+    curl -sS $(get_latest_pypi_link virtualenv .tar.gz) > virtualenv.tar.gz
+    tar xf virtualenv.tar.gz
+    pushd virtualenv-*
+    python virtualenv.py "$VENV_VENV"
+    popd
+    "$VENV_VENV"/bin/pip install virtualenv.tar.gz
+    "$VENV_VENV"/bin/pip install virtualenvwrapper
+    rm -rf virtualenv*
+
+    if grep -q VIRTUALENV ~/.bashrc
+    then
+        echo "virtualenv config already in .bashrc"
+    else
+        echo "adding virtualenv config to .bashrc"
+        add_to_bashrc
+    fi
+fi
